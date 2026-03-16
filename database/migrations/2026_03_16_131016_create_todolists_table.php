5ab59e834dd2e9d506c9d5b24d04e58b2671a98e
sangij24001-admin <san-j24001@sangi.jp>
2026-03-16 23:22:50 +0900

    Todoリストテーブル（todolists）のマイグレーションファイル作成 & マイグレーション実行 テーブル作成をターミナル-mysqlから確認

--- database/migrations/2026_03_16_131016_create_todolists_table.php
+++ database/migrations/2026_03_16_131016_create_todolists_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('todolists', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->String('content');
+            $table->date('daadline');
+            $table->boolean('status')->default(0);  // 完了なら1,未完なら0
             $table->timestamps();
         });
     }
