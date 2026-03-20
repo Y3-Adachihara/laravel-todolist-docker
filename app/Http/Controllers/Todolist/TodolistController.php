9d25cb29f4a3bb3bd6cf8eb7ab0ba45bba70fb4b
sangij24001-admin <san-j24001@sangi.jp>
2026-03-20 18:46:52 +0900

    コントローラーを作成

---
+++ app/Http/Controllers/Todolist/TodolistController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Todolist;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Todolist;
+
+class TodolistController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $todolists = Todolist::where([['status', '=', 0]])->get();
+        return view('todolist.todolist')->with([
+            'todolists' => $todolists]);
+    }
+}
