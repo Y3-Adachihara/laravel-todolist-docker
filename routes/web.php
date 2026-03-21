617a50607e7cd07727f6bef8822f3ea66386521a
sangij24001-admin <san-j24001@sangi.jp>
2026-03-21 21:43:45 +0900

    Todolistルートを追加

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Todolist\TodolistController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+ROute::get('todolist', TodolistController::class)
+    ->name('todolist');
