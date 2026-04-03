cf6c7fe9fe476faf8aa7a3392900738c2d960c81
sangij24001-admin <san-j24001@sangi.jp>
2026-04-03 23:16:04 +0900

    button-check_学習用コメント追加

--- resources/views/components/element/button-check.blade.php
+++ resources/views/components/element/button-check.blade.php
@@ -1,3 +1,12 @@
+{{--
+    ・route()はURLメーカーのようなもの。name()で定義した文字列を引数として指定すると、Route::put()の第一引数がURL文字列として置き換えられる。
+
+    ・formタグでmethod="POST"と指定しているのに@method('PUT')としているのは、formで扱えるメソッドとLaravel側で使いたいメソッドの違いによるもの。
+    　formタグはPOSTとGETのみ使用できるが、Laravelではデータの更新処理をPUTやPATCHで行いたいというルールがある。
+    　HTML側ではmethod="POST"としておき、隠しコマンドとして@method='PUT'を入れることで、<input type="hidden" name="_method" value="PUT">という隠し属性が生成され、Laravel側でRoute::put()で定義したルーティングに振り分けられる。
+    　→ メソッドパディングと呼ぶ
+--}}
+
 <form action="{{ route('checktodo', ['todoId' => $todoId]) }}" method="POST">
     @method('PUT')
     @csrf
