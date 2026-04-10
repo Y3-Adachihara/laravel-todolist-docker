730a12294be2d78e2b3278e7f4a6c08670e25c81
sangij24001-admin <san-j24001@sangi.jp>
2026-04-10 23:51:38 +0900

    学習用コメント追加1

--- app/Http/Controllers/Todolist/EditTodo/EditTodoPageController.php
+++ app/Http/Controllers/Todolist/EditTodo/EditTodoPageController.php
@@ -13,6 +13,14 @@ class EditTodoPageController extends Controller
      */
     public function __invoke(Request $request)
     {
+        // ここでのroute()は、formタグのaction属性で書いたroute()とは別の動きをする。
+        // あっちは、URLを生成するのが主な仕事。->name()の引数で指定した名前をroute()の引数として指定し、そのルーティングを呼び出す
+        // 対してこっちは、URLの{}で指定したパラメータを取り出す役割がある。requestやcontrollerの中で使われる
+        /* 
+            ①todolist.blade.phpから編集ボタンが押され、todoIdという名前のパラメータがくっついたURLを受け取る（route()によってURLが生成された）。
+            ②それを基にルーティングが行なわれる。今回は、{todoId}というパラメータをくっつける
+            ③$request->route('todoId')の部分で、URLにくっついているパラメータを参照し、保持させる。
+        */
         $todoId = (int) $request->route('todoId');
         $todo = Todolist::where('id', $todoId)->firstOrFail();
         return view('todolist.edittodo')->with('todo', $todo);
