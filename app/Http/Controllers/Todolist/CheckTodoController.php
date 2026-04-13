2263407c1208b2b23c5f626d1657cc4fa2552c1e
sangij24001-admin <san-j24001@sangi.jp>
2026-04-13 21:33:34 +0900

    Todoチェック機能のコントローラを作成

---
+++ app/Http/Controllers/Todolist/CheckTodoController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Todolist;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use APP\Models\Todolist;
+
+class CheckTodoController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $todoId = $request->route('todoId');
+        $todo = Todolist::where('id', $todoId)->firstOrFail();
+        $todo->status = 1;
+        $todo->save();
+        return redirect()->route('todolist');
+    }
+}
