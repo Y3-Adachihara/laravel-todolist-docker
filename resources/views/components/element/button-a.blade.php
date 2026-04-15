c4d796deb4969e9b8fdc7a450001a262e0f257b7
sangij24001-admin <san-j24001@sangi.jp>
2026-04-15 10:20:47 +0900

    button-aのエラーに際して、デバッグの教訓を書いた学習用コメントを追加

--- resources/views/components/element/button-a.blade.php
+++ resources/views/components/element/button-a.blade.php
@@ -7,6 +7,9 @@
     ＠php以外のコメントはこの記号を使う。必ず＠propsを先頭に来させる。
     親ビュー（todolist.blade.php）からデータを受け取るための受け取り窓口が＠props
     生PHPの時のように＠phpの中で初期化の宣言をしてしまうと、親ビューからhrefなどの値が受け取れなくなってしまい、全てこのbutton-a.blade.phpの中で宣言した色や遷移先のボタンしか作れなくなってしまう
+
+    ボタンでエラーが生じていた時、このコンポーネントを使っている箇所でエラーが起きるため、表示できているビューに置かれているコンポーネントは正常。
+    そのため、このボタンを含め、コンポーネントでエラーが生じていたら、エラーが生じているビューを見つけることが第一。
 --}}
 
 @php
@@ -17,7 +20,7 @@
                 'add' => 'py-2 px-4 text-white bg-blue-500 hover:bg-blue-600 focus:ring-blue-500',
                 'undo' => 'py-2 px-4 text-white bg-green-500 hover:bg-green-600 focus:ring-green-500',
                 'edit' => 'py-4 px-4 text-white bg-green-500 hover:bg-green-600 focus:ring-green-500',
-                'default' => '',
+                default => '',
             };
         }
     }
