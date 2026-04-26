f09ece502eb9615b04dc39cfa8edaf3ce89e17f4
a-yuta <san-j24001@sangi.jp>
2026-04-26 23:23:15 +0900

    本番環境でHTTPS強制の接続をさせるよう設定

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // productionは本番環境を意味する
+        // === は値とデータ型の両方が等しいか（完全一致）を判定する演算子（厳格比較演算子）
+        if (config('app.env') === 'production') {
+            // .envファイルのAPP_ENVがproduction（本番環境）だった時、URLを全てhttps://にする。
+            URL::forceScheme('https');
+        }
     }
 }
